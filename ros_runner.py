6fa1dbd47a3300c72f03e3d29d63ffbe64cf2c10
Nikolaus Wagner <nikolaus.wagner@protonmail.com>
2024-07-16 20:39:21 +0200

    fixes

--- ros_runner.py
+++ ros_runner.py
@@ -267,7 +267,8 @@ class SAM6DRunner(object):
 
     msg_seg = self.cam_manager.bridge.cv2_to_imgmsg(img_mask, encoding="bgr8")
     self.pub_vis_seg.publish(msg_seg)
-    msg_idx = cv2.applyColorMap(img_idx, cv2.COLORMAP_JET)
+    msg_idx = cv2.applyColorMap(img_idx.astype(np.uint8), cv2.COLORMAP_JET)
+    msg_idx = self.cam_manager.bridge.cv2_to_imgmsg(img_mask, encoding="bgr8")
     self.pub_vis_idx.publish(msg_idx)
 
     resp.full_pcl = convert_rgbd_to_pc2(self.cam_manager.rgb,
